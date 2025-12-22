7674108d2c8bba232e3f5ea1d133843c85ebdd7c
unknown <felipescherer25@gmail.com>
2025-12-22 18:25:44 -0300

    :sparkles: feat lint-project

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,37 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
+import stylistic from '@stylistic/eslint-plugin';
+import eslintPluginBetterTailwindcss from 'eslint-plugin-better-tailwindcss';
+import jsonc from 'eslint-plugin-jsonc';
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
+  ...jsonc.configs['flat/recommended-with-jsonc'],
+
+  {
+    plugins: {
+      '@stylistic': stylistic,
+      'better-tailwindcss': eslintPluginBetterTailwindcss,
+      'jsonc': jsonc,
+    },
+    rules: {
+      ...stylistic.configs.recommended.rules,
+      '@stylistic/semi': ['error', 'always'],
+      'better-tailwindcss/enforce-consistent-class-order': 'warn',
+      'better-tailwindcss/enforce-shorthand-classes': 'warn',
+      'better-tailwindcss/no-duplicate-classes': 'warn',
+      'better-tailwindcss/no-deprecated-classes': 'warn',
+      'better-tailwindcss/no-unnecessary-whitespace': 'warn',
+      'jsonc/indent': ['error', 4],
+    },
+  },
   globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
   ]),
 ]);
 
